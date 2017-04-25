3bea3d4f08e0a079518ab45baaabdcbfde055696
dc-park <cdpark@andrew.cmu.edu>
2017-04-25 17:23:10 -0400

    delete

--- prediction_code.R
+++
@@ -1,8 +0,0 @@
-setwd("~/CMU/4 Senior Year/Spring/Data Mining/final project")
-
-# load csv files
-flights2015 <- read.csv('flights2015.csv')
-flights2016 <- read.csv('flights2016.csv')
-flights2016_visible <- read.csv('flights2016_visible.csv')
-flights2016_guess <- read.csv('flights2016_guess.csv')
-
