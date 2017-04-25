fee600e7b443f0e12fc8d743f4d007a67e4ec493
dc-park <cdpark@andrew.cmu.edu>
2017-04-25 17:19:46 -0400

    add code
    added prediction code

---
+++ prediction_code.R
@@ -0,0 +1,8 @@
+setwd("~/CMU/4 Senior Year/Spring/Data Mining/final project")
+
+# load csv files
+flights2015 <- read.csv('flights2015.csv')
+flights2016 <- read.csv('flights2016.csv')
+flights2016_visible <- read.csv('flights2016_visible.csv')
+flights2016_guess <- read.csv('flights2016_guess.csv')
+
